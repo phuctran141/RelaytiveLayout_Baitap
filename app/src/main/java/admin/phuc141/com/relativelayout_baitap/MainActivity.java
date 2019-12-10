5a5a9bfcd96fd783da8cd701795593c93d975ca8
unknown <tranvanphuc141@gmail.com.vn>
2019-12-10 18:59:41 +0700

    Baitap1

---
+++ app/src/main/java/admin/phuc141/com/relativelayout_baitap/MainActivity.java
@@ -0,0 +1,14 @@
+package admin.phuc141.com.relativelayout_baitap;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
